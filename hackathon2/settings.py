4ac0f9151de7885e9143868de02250c1c09ebefe
MiDev <razafintsalamamikaia@gmail.com>
2022-09-07 12:48:14 +0400

    new app create and database connected

--- hackathon2/settings.py
+++ hackathon2/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'hackathon2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Online Garage Sale',
+        'USER': 'postgres',
+        'PASSWORD': 'Lionirod77#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
